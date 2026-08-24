70158109f9f7e5e07c0ae27d0bc61e1fe925a1d6
Henry Gardella <hwgardella@gmail.com>
2026-08-24 14:06:42 -0700

    Update decks page styling

--- svelte-app/src/routes/decks/+page.svelte
+++ svelte-app/src/routes/decks/+page.svelte
@@ -1274,7 +1274,7 @@
 		background: rgba(0, 0, 0, 0.25);
 		border-top: 1px solid rgba(0, 0, 0, 0.85);
 		box-shadow:
-			inset 0 0 12px 8px rgba(43, 43, 43, 0.5),
+			inset 0 0 12px 8px rgba(32, 32, 32, 0.5),
 			inset 0.5px 1px 3px rgba(255, 255, 255, 0.18),
 			inset -0.5px -1px 3px rgba(0, 0, 0, 0.75);
 		display: flex;
